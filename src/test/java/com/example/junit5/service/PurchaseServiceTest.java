b994e55e0fb4b2adb1a8acb8e98836f9790d3e19
Maada Loukmane <l.maada@mytower.fr>
2024-07-03 22:24:12 +0100

    Nested Test

--- src/test/java/com/example/junit5/service/PurchaseServiceTest.java
+++ src/test/java/com/example/junit5/service/PurchaseServiceTest.java
@@ -6,100 +6,105 @@ import com.example.junit5.model.Invoice;
 import com.example.junit5.model.User;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
+@DisplayName("Purchase Service Tests")
 public class PurchaseServiceTest {
 
-    @Test
-    @Disabled("This test is disabled because of bug #S2P-42")
-    @DisplayName("Should return half the invoiced amount when user is premium")
-    void premiumUserHalfInvoicedAmount() {
+    @Nested
+    @DisplayName("Invoiced Amount Tests")
+    class InvoicedAmountTests {
 
-        // given
-        PurchaseService purchaseService = new PurchaseService();
-        var user = new User(1, "loukmane@gmail.com", true);
-        var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
+        @Test
+        @DisplayName("Should return half the invoiced amount when user is premium")
+        void premiumUserHalfInvoicedAmount() {
+            // given
+            PurchaseService purchaseService = new PurchaseService();
+            var user = new User(1, "loukmane@gmail.com", true);
+            var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
 
-        // when
-        var invoicedAmount = purchaseService.getInvoicedAmount(user, invoice);
+            // when
+            var invoicedAmount = purchaseService.getInvoicedAmount(user, invoice);
 
-        // then
-        assertAll("invoicedAmount checks",
-                () -> assertNull(invoicedAmount, () -> {
-                    // Simulate calling an external API for message construction
-                    // This operation is only performed if the assertion fails
-                    return "invoicedAmount should never be null";
-                }),
-                () -> assertEquals(new BigDecimal("51.00"), invoicedAmount, "invoicedAmount should be half the invoice amount")
-        ); // should fail
+            // then
+            assertAll("invoicedAmount checks",
+                    () -> assertNotNull(invoicedAmount, () -> {
+                        // Simulate calling an external API for message construction
+                        return "invoicedAmount should never be null";
+                    }),
+                    () -> assertEquals(new BigDecimal("50.00"), invoicedAmount, "invoicedAmount should be half the invoice amount")
+            );
+        }
+
+        @Test
+        @DisplayName("Should return the invoiced amount when user is not premium")
+        void nonPremiumUserFullInvoicedAmount() {
+            // given
+            PurchaseService purchaseService = new PurchaseService();
+            var user = new User(1, "loukmane@gmail.com", false);
+            var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
+
+            // when
+            var invoicedAmount = purchaseService.getInvoicedAmount(user, invoice);
+
+            // then
+            assertNotNull(invoicedAmount, "invoicedAmount should never be null");
+            assertEquals(new BigDecimal("100.0"), invoicedAmount, "invoicedAmount should be equal to the invoice amount");
+        }
     }
 
-    @Test
-    @DisplayName("Should return the invoiced amount when user is not premium")
-    void nonPremiumUserFullInvoicedAmount() {
+    @Nested
+    @DisplayName("Shipping Amount Tests")
+    class ShippingAmountTests {
 
-        // given
-        PurchaseService purchaseService = new PurchaseService();
-        var user = new User(1, "loukmane@gmail.com", false);
-        var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
+        @Test
+        @DisplayName("Should return zero shipping amount when user is premium")
+        void premiumUserZeroShippingAmount() {
+            // given
+            PurchaseService purchaseService = new PurchaseService();
+            var user = new User(1, "loukmane@gmail.com", true);
+            var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
 
-        // when
-        var invoicedAmount = purchaseService.getInvoicedAmount(user, invoice);
+            // when
+            var shippingAmount = purchaseService.getShippingAmount(user, invoice);
 
-        // then
-        assertNotNull(invoicedAmount, "invoicedAmount should never be null");
-        assertEquals(new BigDecimal("100.0"), invoicedAmount, "invoicedAmount should be equal to the invoice amount");
-    }
+            // then
+            assertNotNull(shippingAmount, "shippingAmount should never be null");
+            assertEquals(new BigDecimal("0.0"), shippingAmount, "shippingAmount should be zero");
+        }
 
-    @Test
-    @DisplayName("Should return zero shipping amount when user is premium")
-    void premiumUserZeroShippingAmount() {
+        @Test
+        @DisplayName("Should return zero shipping amount when invoice is not rushed delivery")
+        void nonRushedDeliveryZeroShippingAmount() {
+            // given
+            PurchaseService purchaseService = new PurchaseService();
+            var user = new User(1, "loukmane@gmail.com", false);
+            var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
 
-        // given
-        PurchaseService purchaseService = new PurchaseService();
-        var user = new User(1, "loukmane@gmail.com", true);
-        var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
+            // when
+            var shippingAmount = purchaseService.getShippingAmount(user, invoice);
 
-        // when
-        var shippingAmount = purchaseService.getShippingAmount(user, invoice);
+            // then
+            assertNotNull(shippingAmount, "shippingAmount should never be null");
+            assertEquals(new BigDecimal("0.0"), shippingAmount, "shippingAmount should be zero");
+        }
 
-        // then
-        assertNotNull(shippingAmount, "shippingAmount should never be null");
-        assertEquals(new BigDecimal("0.0"), shippingAmount, "shippingAmount should be zero");
-    }
+        @Test
+        @DisplayName("Should return 20% of the invoiced amount as shipping amount when invoice is rushed delivery and user is not premium")
+        void nonPremiumUserRushedDeliveryShippingAmount() {
+            // given
+            PurchaseService purchaseService = new PurchaseService();
+            var user = new User(1, "loukmane@gmail.com", false);
+            var invoice = new Invoice(1, new BigDecimal("100.0"), 1, true);
 
-    @Test
-    @DisplayName("Should return zero shipping amount when invoice is not rushed delivery")
-    void nonRushedDeliveryZeroShippingAmount() {
+            // when
+            var shippingAmount = purchaseService.getShippingAmount(user, invoice);
 
-        // given
-        PurchaseService purchaseService = new PurchaseService();
-        var user = new User(1, "loukmane@gmail.com", false);
-        var invoice = new Invoice(1, new BigDecimal("100.0"), 1, false);
-
-        // when
-        var shippingAmount = purchaseService.getShippingAmount(user, invoice);
-
-        // then
-        assertNotNull(shippingAmount, "shippingAmount should never be null");
-        assertEquals(new BigDecimal("0.0"), shippingAmount, "shippingAmount should be zero");
-    }
-
-    @Test
-    @DisplayName("Should return 20% of the invoiced amount as shipping amount when invoice is rushed delivery and user is not premium")
-    void nonPremiumUserRushedDeliveryShippingAmount() {
-
-        // given
-        PurchaseService purchaseService = new PurchaseService();
-        var user = new User(1, "loukmane@gmail.com", false);
-        var invoice = new Invoice(1, new BigDecimal("100.0"), 1, true);
-
-        // when
-        var shippingAmount = purchaseService.getShippingAmount(user, invoice);
-
-        // then
-        assertNotNull(shippingAmount, "shippingAmount should never be null");
-        assertEquals(new BigDecimal("20.00"), shippingAmount, "shippingAmount should be 20% of the invoice amount");
+            // then
+            assertNotNull(shippingAmount, "shippingAmount should never be null");
+            assertEquals(new BigDecimal("20.00"), shippingAmount, "shippingAmount should be 20% of the invoice amount");
+        }
     }
 }
