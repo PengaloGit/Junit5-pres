8dfbdb75f7e0326fd7aef2eb380f0c879a175d22
Maada Loukmane <l.maada@mytower.fr>
2024-07-10 19:59:21 +0100

    Time assertions

--- src/test/java/com/example/junit5/service/ServiceTest.java
+++ src/test/java/com/example/junit5/service/ServiceTest.java
@@ -4,6 +4,14 @@ import com.example.junit5.annotation.IgnoreOnLocalEnv;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
 public class ServiceTest {
 
     @Test
@@ -12,7 +20,23 @@ public class ServiceTest {
     void algoPerformanceTest() throws InterruptedException {
         Thread.sleep(1000);
         System.out.println("Test finished");
-        // I don't want this test to depend on the devs machine specs
-        // I want this test to on ly be excuted on the CI/CD pipeline
+        // I don't want this test to depend on the devs' machine specs
+        // I want this test to only be executed when in the CI/CD pipeline
+    }
+    
+    @Test
+    @DisplayName("Should exceed timeout of 10 ms and wait for task completion")
+    void testTimeoutExceeded() {
+        assertTimeout(ofMillis(10), () -> {
+            Thread.sleep(100);
+        });
+    }
+
+    @Test
+    @DisplayName("Should exceed timeout of 10 ms and prematurely terminate task")
+    void timeoutExceededWithPreemptiveTermination() {
+        assertTimeoutPreemptively(ofMillis(10), () -> {
+            Thread.sleep(100);
+        });
     }
 }
